559aaa98ce205f7715ef437710fe8fe4215f6209
Ricardo <rfiori@users.noreply.github.com>
2024-12-28 02:00:04 -0300

    Merger

---
+++ src/IP.Geolocation/GeoPlugin.cs
@@ -0,0 +1,24 @@
+﻿using IP.Geolocation.APIReturns;
+using IP.Geolocation.Interfaces;
+using System.Text.Json;
+
+namespace IP.Geolocation
+{
+	public partial class FindIP
+	{
+		private async Task<IIPGeolocationResult?> getGeopluginAsync(string ip, int timeOut = TIMEOUT_MILESSEC)
+		{
+			// api http://www.geoplugin.net
+			const string API_Geoplugin_URL = "http://geoplugin.net/json.gp?ip=";
+			try
+			{
+				var content = await CallAPI($"{API_Geoplugin_URL}{ip}");
+				if (!string.IsNullOrEmpty(content))
+					return JsonSerializer.Deserialize<GeopluginReturn>(content);
+				else
+					return null;
+			}
+			catch { return null; }
+		}
+	}
+}
---
+++ src/IP.Geolocation/IPInfo.cs
@@ -0,0 +1,24 @@
+﻿using IP.Geolocation.APIReturns;
+using IP.Geolocation.Interfaces;
+using System.Text.Json;
+
+namespace IP.Geolocation
+{
+	public partial class FindIP
+	{
+		private async Task<IIPGeolocationResult?> getFromIPInfoAsync(string ip, int timeOut = TIMEOUT_MILESSEC)
+		{
+			// api https://ipinfo.io/8.8.8.8/json
+			const string API_Geoplugin_URL = "https://ipinfo.io";
+			try
+			{
+				var content = await CallAPI($"{API_Geoplugin_URL}/{ip}/json");
+				if (!string.IsNullOrEmpty(content))
+					return JsonSerializer.Deserialize<IPInfoReturn>(content);
+				else
+					return null;
+			}
+			catch { return null; }
+		}
+	}
+}
